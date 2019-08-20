03849a1dfbd2aaa00a637817ba3f118c3102c109
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-20 21:20:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,15 +3,17 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-  timeNow = Time.now.strftime("%H:%M")
-  resp.write "Time Now #{timeNow}"
-#    if num_1==num_2 && num_2==num_3
-#      resp.write "You Win"
-#    else
-#      resp.write "You Lose"
-#    end
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
  
-#    resp.finish
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+ 
+    resp.finish
   end
  
 end
