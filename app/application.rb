80606483094606943c600ec90fc0a5d26ac9297c
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-20 21:16:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-  
- 
+  timeNow = Time.now.strftime("%H:%M")
+  resp.write timeNow
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else
