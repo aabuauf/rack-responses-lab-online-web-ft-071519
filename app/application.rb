c0fc7cef838b75424d9fb44e19639f5f00741134
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-20 21:17:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,13 +5,13 @@ class Application
  
   timeNow = Time.now.strftime("%H:%M")
   resp.write timeNow
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
+#    if num_1==num_2 && num_2==num_3
+#      resp.write "You Win"
+#    else
+#      resp.write "You Lose"
+#    end
  
-    resp.finish
-  end
+#    resp.finish
+#  end
  
 end
